877527711181b9e6183a589021fb90a57a0b4f05
Michael Woolweaver <m.woolweaver@icloud.com>
2019-04-15 23:19:34 -0500

    Update pihole_tweeter.py

--- pihole_tweeter.py
+++ pihole_tweeter.py
@@ -66,7 +66,7 @@ def get_pihole_data():
 
 def construct_tweet(data):
     today = datetime.today().strftime("%m/%d/%Y")
-    tweet += 'Pi-hole statistics for {date}:\n'.format(date=today)
+    tweet = 'Pi-hole statistics for {date}:\n'.format(date=today)
     tweet += 'Ads Blocked: ' + str(comma_value(data['ads_blocked_today']))
     tweet += ' (' + str(round(data['ads_percentage_today'], 2)).replace('.', ',') + ' %)\n'
     tweet += 'Total DNS Queries: ' + str(comma_value(data['dns_queries_today'])) + '\n'
