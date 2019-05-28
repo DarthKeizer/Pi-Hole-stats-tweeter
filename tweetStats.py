d80e7b059800b20b089f86a09d78f3bb550a22e7
mwoolweaver <m.woolweaver@icloud.com>
2019-05-28 02:04:47 -0500

    move to threader

--- tweetStats.py
+++ tweetStats.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from lib import get_config as cfg # where all the config data lives
+from lib.get_config import get_cfgt as cfgt # where all the config data lives
 from lib.get_api import get_api as ga # where we interact with the Twitter API
 import lib.construct_tweet as ct # where the tweet is put together
 from lib.debug import debug_tweet, d1, s # All the deugging happens here && parses for passed arguements
+
+from TwitterAPI import TwitterAPI
+from threader import Threader
 
 # Tweet it!
 def tweet_it(api):
@@ -12,25 +15,17 @@ def tweet_it(api):
     PHtweet = ct.construct_tweet(ct.pi(), ct.si())[0]
     SYtweet = ct.construct_tweet(ct.pi(), ct.si())[1]    
     # and send it
-    try:
-        status = api[0].update_status(status=PHtweet)
-    except api[2].TweepError as e: # most of the time you end up here cause your tweet was to long ( tweet > 280 characters)
-        print(e.reason)            #  if this happens regularly you might have to remove some data from the tweet
-        s.switch(1)
-        return
-    print('Tweet tweeted!! --> https://twitter.com/' + status.author.screen_name + '/status/' + status.id_str)
 
-    piStatus = status.id_str
-    me = status.author.screen_name
-    reply_status = "@%s @The_Pi_Hole @GoogleCompute\n %s" % (me, SYtweet)
+    keys = dict(consumer_key=cfgt()[0],
+            consumer_secret=cfgt()[1],
+            access_token_key=cfgt()[2],
+            access_token_secret=cfgt()[3])
+    api = TwitterAPI(**keys)
 
-    try:
-        status = api[0].update_status(status=reply_status, in_reply_to_status_id=piStatus)
-    except api[2].TweepError as e: # most of the time you end up here cause your tweet was to long ( tweet > 280 characters)
-        print(e.reason)            #  if this happens regularly you might have to remove some data from the tweet
-        s.switch(1)
-        return
-    print('Tweet tweeted!! --> https://twitter.com/' + status.author.screen_name + '/status/' + status.id_str)
+    tweets = [PHtweet, SYtweet]
+    th = Threader(tweets, api, wait=2)
+    th.send_tweets()
+
 
 # Make it Happen!!
 def main():
@@ -39,7 +34,7 @@ def main():
     if d != 0: # checks for any passed args
         s.switch(d) 
     else: # if no args send it!!
-        tweet_it(ga(cfg.get_cfgt()))
+        tweet_it(ga(cfgt()))
 
 # action really happens down here tho
 if __name__ == '__main__':
