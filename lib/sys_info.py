2db56e726bf98b000611e0294ff7f8cb2f0f6a2b
mwoolweaver <m.woolweaver@yahoo.com>
2019-07-20 06:17:45 -0500

    update requirements.txt for most recent changes

--- lib/sys_info.py
+++ lib/sys_info.py
@@ -46,7 +46,6 @@ def sys_info():
     hddStats = usedGB + '/' + totalGB + '|' + percentHDD + '%' # sys_info[4] - All hdd stats in 1 variable
     kernelOS = pl() # sys_info[5] - Kernel version && OS version
     OSN = OSname(pretty=False)
-    print(OSN)
     ul = str(uls) + " Mbps"
     dl = str(dls) + " Mbps"
     ping = str(pings) + " ms"
