89de70465b1dc1147ef8458d28fdac765dfe4d67
mwoolweaver <m.woolweaver@yahoo.com>
2019-07-21 23:31:05 -0500

    add debug variable explination
    and more output

--- lib/debug.py
+++ lib/debug.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from lib.sys_info import sys_info as si # where system information is gathered
-from lib.pihole_info import pihole_info as pi # where pihole information is gathered
-from lib.speed_test import us, ds, pg, isp, share, dlByte, ulByte, data
-import lib.construct_tweet as ct # where the tweet is put together
+
 import lib.get_config as apiC # where all the config data lives
-from lib.get_api import get_api as apiT # where we interact with the Twitter API
 from emoji import UNICODE_EMOJI # where we get our emoji dictionary from
 from argparse import ArgumentParser # how we parse command line when/if they are passed
 
@@ -26,8 +22,9 @@ d1 = db + dbl + dbp # add our args together for better handling of each case
 class debugSwitch:
     
     # Print number used to determine debug output
+    print("Debug Variable")
     print(d1)
-    
+
     # Where the switching happens    
     def switch(self, dbm):
         return getattr(self, 'case_' + str(dbm), lambda: parser.print_help())()
@@ -35,42 +32,64 @@ class debugSwitch:
     def case_1(self): # Print test tweet with real values
         return debug_tweet()
     def case_3(self): # Check Twitter login
+        from lib.get_api import get_api as apiT # where we interact with the Twitter API
         return apiT(apiC.get_cfgt())
     def case_4(self): # Print Test Tweet && Verify Twitter Login
+        from lib.get_api import get_api as apiT # where we interact with the Twitter API
         return debug_tweet(), apiT(apiC.get_cfgt())
     def case_5(self): # Check pi-hole api reachability
+        from lib.pihole_info import pihole_info as pi # where pihole information is gathered
         return print('if ' + str(pi()[9]) + ' == 200 --> success \n\n Otherwise pi-hole URL is not configured properly. \n Check config.json.\n')
     def case_6(self): # Print Test Tweet && Check pi-hole api reachability
+        from lib.pihole_info import pihole_info as pi # where pihole information is gathered
         return debug_tweet(), print('if ' + str(pi()[9]) + ' == 200 --> success \n\n Otherwise pi-hole URL is not configured properly. \n Check config.json.\n')
     def case_8(self): # Check Twitter login && Check pi-hole api reachability
+        from lib.get_api import get_api as apiT # where we interact with the Twitter API
+        from lib.pihole_info import pihole_info as pi # where pihole information is gathered
         return apiT(apiC.get_cfgt()), print('if ' + str(pi()[9]) + ' == 200 --> success \n\n Otherwise pi-hole URL is not configured properly. \n Check config.json.\n')
     def case_9(self): # Print All The Above Debug Options
-        return debug_tweet(), apiT(apiC.get_cfgt()), print('if ' + str(pi()[9]) + ' == 200 --> success \n\n Otherwise pi-hole URL is not configured properly. \n Check config.json.\n')
+        return debug_tweet()
 
 s = debugSwitch()
 
 # prints a test tweet and most (if not all) information used by this program
 def debug_tweet():
+
+    print('\n Twitter Keys')
+    print(apiC.get_cfgt()[5])
+
+    print("\nCheck Twitter Login")
+    from lib.get_api import get_api as apiT # where we interact with the Twitter API
+    print(apiT(apiC.get_cfgt()))
     
+    print('\n Pihole Address')
+    print(apiC.get_cfgp()[1])
+    
+    print("\nPiHole Status")
+    from lib.pihole_info import pihole_info as pi # where pihole information is gathered
+    print('if ' + str(pi()[9]) + ' == 200 --> success \n\n Otherwise pi-hole URL is not configured properly. \n Check config.json.\n')
+    
+    print('\n Pihole Stats')
+    from lib.pihole_info import pihole_info as pi # where pihole information is gathered
+    print(pi())
+    
+    print('\n SpeedTest Info')
+    from lib.speed_test import data
+    print(data)
+    
+    print('\n System Stats')
+    from lib.sys_info import sys_info as si # where system information is gathered
+    print(si())
+    
+    print('\n The tweets that where created.')
+    import lib.construct_tweet as ct # where the tweet is put together
     # build tweet
     PHtweet = ct.construct_tweet(ct.pi(), ct.si())[0]
     SYtweet = ct.construct_tweet(ct.pi(), ct.si())[1]
     Nettweet = ct.construct_tweet(ct.pi(), ct.si())[2]
-
     tweet = '\n\n Tweet 1\n' + PHtweet + '\n\n Tweet 2\n' + SYtweet + '\n\n Tweet 3\n' + Nettweet + '\n'
-
-    print('\n Twitter Keys')
-    print(apiC.get_cfgt()[5])
-    print('\n Pihole Address')
-    print(apiC.get_cfgp()[1])
-    print('\n Pihole Stats')
-    print(ct.pi())
-    print('\n System Stats')
-    print(ct.si())
-    print('\n SpeedTest Info')
-    print(data)
-    print('\n The tweets that where created.')
     print(tweet)
+    
     print('\n Number of characters in tweet +/- 1 or 2') # will try and nail this down to a more accurate number
     num_emoji = (sum(tweet.count(emoji) for emoji in UNICODE_EMOJI)) # accurately count and track emoji
     ignored_chars = UNICODE_EMOJI.copy() # thanks to https://stackoverflow.com/q/56214183/11456464
