0fdf0e8bd432bbde7e527413e24dc38bf463aed5
mwoolweaver <m.woolweaver@yahoo.com>
2019-07-24 02:30:32 -0500

    unneeded import

--- lib/sys_info.py
+++ lib/sys_info.py
@@ -3,7 +3,6 @@
 # retreive data for system information
 
 def sys_info():
-    import platform
     from platform import platform as pl  # used to retreive OS && kernel version
     from distro import name as OSname # used to retreive OS name for hashtag
     from re import sub as re # used to apply regex
