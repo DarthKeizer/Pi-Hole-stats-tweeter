cbecc6430aed4d18083bf4e681184e22def13b9c
mwoolweaver <m.woolweaver@yahoo.com>
2019-08-11 03:46:34 -0500

    remove info that was repeated

--- lib/construct_tweet.py
+++ lib/construct_tweet.py
@@ -30,7 +30,6 @@ def construct_tweet(ph, sy):
 
     # Third Tweet
     Nettweet = '#NetStats'
-    Nettweet += '\nNetwork Interfaces: ' + sy[3] # network interface names (doesn't include the loopback interface)
     Nettweet += '\nPing: ' + sy[8] # Ping via speedtest-cli
     Nettweet += '\nSpeed Achieved (dl/ul): ' + sy[7] + '/' + sy[6] # Upload speed via speedtest-cli
     Nettweet += '\nData Used (dl/ul): ' + sy[11] + '/' + sy[12]
