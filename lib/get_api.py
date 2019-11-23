24e4aa007a0873f201864dc9ece2b5e9359a18cf
Michael Woolweaver <mwoolweaver@users.noreply.github.com>
2019-11-23 14:00:32 -0600

    variable name

--- lib/get_api.py
+++ lib/get_api.py
@@ -3,12 +3,14 @@
 
 from lib.get_config import get_cfgt as cfgt
 from TwitterAPI import TwitterAPI
+
+
 # login to Twitter
 def get_api():
 
     keys = cfgt()
     
-    KEYS_and_API = TwitterAPI(**keys[4])
+    KEYS_and_API = TwitterAPI(**keys)
     
     verify = KEYS_and_API.request('account/verify_credentials')
     
